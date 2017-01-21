f7f794ac3705bf782b023549691fc6e0e808ecc5
Fabio Mosca <Gounemond@gmail.com>
2017-01-21 22:03:44 +0100

    Changed some prefabas

---
+++ Assets/Scripts/CopyRotation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CopyRotation : MonoBehaviour
+{
+    public bool invertRotation = false;
+    public Transform objectToCopy;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (invertRotation)
+        {
+            this.transform.rotation = objectToCopy.rotation * Quaternion.Euler(0,-1,0);
+        }
+        else
+        {
+            this.transform.rotation = objectToCopy.rotation;
+        }
+    }
+}
