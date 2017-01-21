f6402404f34f4e5c4b57ca7beb624481bb3311ac
Fabio Mosca <Gounemond@gmail.com>
2017-01-21 22:57:20 +0100

    I did something

--- Assets/Scripts/CopyRotation.cs
+++ Assets/Scripts/CopyRotation.cs
@@ -5,23 +5,33 @@ using UnityEngine;
 public class CopyRotation : MonoBehaviour
 {
     public bool invertRotation = false;
+    public bool copyOnlyY = false;
     public Transform objectToCopy;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update ()
+
+    }
+
+    // Update is called once per frame
+    void Update ()
     {
-        if (invertRotation)
+        if (copyOnlyY)
         {
-            this.transform.rotation = objectToCopy.rotation * Quaternion.Euler(0,-1,0);
+            this.transform.rotation = Quaternion.Euler(this.transform.rotation.x, objectToCopy.transform.rotation.y, this.transform.rotation.z);
         }
         else
         {
-            this.transform.rotation = objectToCopy.rotation;
+            if (invertRotation)
+            {
+                this.transform.rotation = objectToCopy.rotation;
+                // THE FUCK HOW DO I INVERT SIGN
+            }
+            else
+            {
+                this.transform.rotation = objectToCopy.rotation;
+            }
         }
+        
     }
 }
